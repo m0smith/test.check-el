6814876f00390000ecf263f27b2931e93ea8bbbd
Matthew O. Smith <matt@m0smith.com>
2015-07-04 12:07:15 -0600

    Implementing through make-size-range-seq

--- generator.el
+++ generator.el
@@ -44,7 +44,7 @@
   (tcel-make-gen
     (lambda (rnd size) value)))
 
-(defun gen-fmap  (k g)
+(defun tcel-gen-fmap  (k g)
   (let ((h (oref g gen))) 
     (tcel-make-gen
      (lambda (rnd size)
@@ -66,6 +66,54 @@ sequences (er, lists)."
   (tcel-make-gen
    (lambda (rnd size)
      (mapcar (lambda (a) (tcel-call-gen a rnd size) gens)))))
+
+
+;; Exported generator functions
+;; ---------------------------------------------------------------------------
+
+(defun tcel-fmap  (f gen)
+  (assert (tcel-generator? gen) t "Second arg to fmap must be a generator")
+  (tcel-gen-fmap (lambda (a) (qc-rt-fmap f a)) gen))
+
+(defun tcel-return (value)
+  "Create a generator that always returns `value`,
+  and never shrinks. You can think of this as
+  the `constantly` of generators."
+  (tcel-gen-pure (qc-rt-pure value)))
+
+(defun tcel-bind-helper  (k)
+  (lambda (rose)
+    (tcel-gen-fmap qc-rt-join
+		   (tcel-make-gen
+		    (lambda (rnd size)
+		      (qc-rt-fmap (lambda (a) (tcel-call-gen a rnd size)
+				    (qc-rt-fmap k rose))))))))
+
+(defun tcel-bind (generator k)
+  "Create a new generator that passes the result of `gen` into function
+  `k`. `k` should return a new generator. This allows you to create new
+  generators that depend on the value of other generators. For example,
+  to create a generator which first generates a vector of integers, and
+  then chooses a random element from that vector:
+      (gen/bind (gen/such-that not-empty (gen/vector gen/int))
+                ;; this function takes a realized vector,
+                ;; and then returns a new generator which
+                ;; chooses a random element from it
+                gen/elements)
+  "
+
+  (assert (generator? generator) t "First arg to bind must be a generator") ;
+  (tcel-gen-bind generator (tcel-bind-helper k)))
+
+(defun tcel-random (&optional seed)
+  (if seed 
+      (random)
+    (random seed)))
+
+(defun tcel-make-size-range-seq  (max-size)
+  (cycle (cljs-el-range 0 max-size)))
+
+
 
 (provide 'generator)
 ;;; generator.el ends here
