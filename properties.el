af5bf2b131c924e97f88bfdb452861681a07c78f
Matthew O. Smith <matt@m0smith.com>
2015-07-09 16:01:24 -0600

    Implemented properties from ClojerScript

---
+++ properties.el
@@ -0,0 +1,51 @@
+;;; properties.el --- Properties from ClojureScript test.check -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Matthew O. Smith
+
+;; Author: Matthew O. Smith <Matthew.Smith@imail.org>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; https://github.com/clojure/test.check/blob/master/src/main/clojure/cljs/test/check/properties.cljs
+
+;;; Code:
+
+(require 'generator)
+
+(defun tcel-properties-apply-gen (fn1)
+  (lambda (args)
+    (let ((result (catch 'properties (apply fn1 args))))
+      (list :result   result
+	    :function fn1
+	    :args     args))))
+
+(defun tcel-properties-for-all*  (args fn2)
+  "Creates a property (properties are also generators). A property
+  is a generator that generates the result of applying the function
+  under test with the realized arguments. Once realized, the arguments
+  will be applied to `function` with `apply`.
+  Example:
+  (for-all* [gen/int gen/int] (fn [a b] (>= (+ a b) a)))
+  "
+  (tcel-generator-fmap
+    (tcel-properties-apply-gen fn2)
+    (apply 'tcel-generator-tuple args)))
+
+
+
+(provide 'properties)
+;;; properties.el ends here
