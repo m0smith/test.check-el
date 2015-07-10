2c13945518acef4b16eb42670e48d0521e5e32df
Matthew O. Smith <matt@m0smith.com>
2015-07-10 09:14:46 -0600

    Added variables

--- tcel-test.el
+++ tcel-test.el
@@ -52,7 +52,32 @@ See https://www.gnu.org/software/emacs/manual/html_node/elisp/Error-Symbols.html
 			   (plist-put options  :num-tests tcel-test-default-test-count)))
         (t (signal 'error (list  "Invalid tcel-test-defspec options: " options)))))
 
+(defcustom tcel-test-report-trials nil
+  "Controls whether property trials should be reported via clojure.test/report.
+  Valid values include:
+  * nil - no reporting of trials (default)
+  * a function - will be passed a clojure.test/report-style map containing
+  :clojure.test.check/property and :clojure.test.check/trial slots
+  * t - provides quickcheck-style trial reporting (dots) via
+  `trial-report-dots`
+  (Note that all reporting requires running `quick-check` within the scope of a
+  clojure.test run (via `test-ns`, `test-all-vars`, etc.)
+  Reporting functions offered by clojure.test.check include `trial-report-dots` and
+  `trial-report-periodic` (which prints more verbose trial progress information
+  every `*trial-report-period*` milliseconds."
+  )
 
+(defcustom  tcel-test-report-shrinking nil
+  "If true, a verbose report of the property being tested, the
+  failing return value, and the arguments provoking that failure is emitted
+  prior to the start of the shrinking search."
+  )
+
+(defcustom tcel-test-trial-report-period 10000
+  "Milliseconds between reports emitted by `trial-report-periodic`."
+  )
+
+(defvar tcel-test-last-trial-report 0)
 
 
 (provide 'tcel-test)
